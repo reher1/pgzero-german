132e5853864eeaa911b13291a53dca57bc21fe9b
nicolaipoehner <nicolai.poehner@uni-wuerzburg.de>
2020-05-20 18:31:50 +0200

    Update german translation

--- doc/conf.py
+++ doc/conf.py
@@ -49,9 +49,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Pygame Zero'
-copyright = '2015, Daniel Pope'
-author = 'Daniel Pope'
+project = 'Pygame Zero german'
+copyright = '2020, Nicolai Poehner'
+author = 'Nicolai Poehner'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
